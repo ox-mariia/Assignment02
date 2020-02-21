21758e713948f0662cc6f0a140b5a3068a9608f3
ox-mariia <60494463+ox-mariia@users.noreply.github.com>
2020-02-20 17:56:04 -0800

    Delete 04-coinflip-streak.js

--- beginning 2/js/04-coinflip-streak.js
+++
@@ -1,9 +0,0 @@
-let coinFlip;
-do {
-       coinFlip = Math.round(Math.random());
-    if (coinFlip === 0) {
-        window.console.log("Heads");
-    } else {
-        window.console.log("Tails");
-    }
-  } while ( coinFlip === 0);
