99ead11da7dbb22a3b61ef4e43059e9c560c2e77
ox-mariia <60494463+ox-mariia@users.noreply.github.com>
2020-02-20 17:55:38 -0800

    Delete 01-larger-or-smaller.js

--- beginning 2/js/01-larger-or-smaller.js
+++
@@ -1,9 +0,0 @@
-let x = Number(prompt("Enter the first number between 1 and 100"));
-let y = Number(prompt("Enter the second number between 1 and 100"));
-if (x > y) {
-    window.document.write("The larger number is " + x);
-} else if (x === y) {
-    window.document.write("The numbers are equal.");
-} else {
-    window.document.write("The larger number is " + y);
-}
