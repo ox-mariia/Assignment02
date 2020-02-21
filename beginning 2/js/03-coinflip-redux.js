ce1d0678c3410211ac302086a8c0e8af87b33000
ox-mariia <60494463+ox-mariia@users.noreply.github.com>
2020-02-20 17:55:55 -0800

    Delete 03-coinflip-redux.js

--- beginning 2/js/03-coinflip-redux.js
+++
@@ -1,9 +0,0 @@
-let coinFlip;
-for (i = 1; i <= 10; i++) {
-    coinFlip = Math.round(Math.random());
-    if (coinFlip === 0) {
-        window.console.log("Heads");
-    } else {
-        window.console.log("Tails");
-    }
-}
