88c2a690c15b0e09242d193b1141151571be3c40
ox-mariia <60494463+ox-mariia@users.noreply.github.com>
2020-03-03 22:31:26 -0800

    Update 09-repeat.js

--- js/09-repeat.js
+++ js/09-repeat.js
@@ -1,1 +1,19 @@
-test
+// MILES CALCULATOR
+let miles, gallons, mpg, again = "y";
+do {
+  miles = parseFloat(window.prompt("Enter miles driven"));
+  gallons = parseFloat(window.prompt("Enter tank size in gallons"));
+  if (!isNaN(miles) && miles> 0 && !isNaN(gallons) && gallons> 0){
+    mpg = miles/gallons;
+    window.document.write("Miles per gallon: " + parseInt(mpg) + "<br>");
+  } else {
+    window.document.write("One or more entries are invalid");
+  }
+  while(true) {
+    again = window.prompt("Another enrty, y/n");
+    if (again === "y" || again === "n") {
+        break;
+    }
+  } 
+}
+while(again === "y");
